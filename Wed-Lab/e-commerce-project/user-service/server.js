ffd9d857bec1d6aa035822076f226fbed7646dcf
Ravikant Tyagi <150049400+sqb-codes@users.noreply.github.com>
2026-09-16 11:24:10 +0530

    Add files via upload

--- Wed-Lab/e-commerce-project/user-service/server.js
+++ Wed-Lab/e-commerce-project/user-service/server.js
@@ -2,7 +2,11 @@ const express = require("express");
 const dotenv = require("dotenv")
 dotenv.config()
 
-const app = express()
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("<h1>Hello, welcome to Docker----</h1>");
+})
 
 const PORT = process.env.PORT || 4545;
 
